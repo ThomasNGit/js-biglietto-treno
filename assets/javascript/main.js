2a849d4bf0ac138aed5a840f0e65568134586c64
ThomasNGit <neronithomas1989@gmail.com>
2019-10-17 13:41:57 +0200

    inseriti i prompt di chilometraggio ed età

---
+++ assets/javascript/main.js
@@ -0,0 +1,16 @@
+// ll programma dovrà chiedere all’utente il numero di chilometri
+// e l’età e calcolare il prezzo del biglietto
+// ci saranno sconti per gli under 18(20%) e over 65(40%)
+
+
+// prompt con chilometri da percorrere
+var chilometri = prompt("Quanti chilometri devi percorrere?");
+
+console.log(chilometri);
+
+// prompt con età dell'user
+var eta = prompt("Quanti anni hai?");
+
+console.log(eta);
+
+// calcolo del costo del biglietto in relazione ai chilometri da percorrere
