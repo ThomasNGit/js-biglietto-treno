cec7cb110e9ff717225008cfc25e959a5a7e6e38
ThomasNGit <neronithomas1989@gmail.com>
2019-10-17 13:57:27 +0200

    inseriti costi del biglietto per over 65 e under 18

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -17,15 +17,23 @@ var eta = prompt("Quanti anni hai?");
 
 console.log(eta);
 
+
 // calcolo del costo del biglietto in relazione ai chilometri da percorrere
 var costo = chilometri * costoalkm;
 
 console.log(costo);
 
+// calcolo costo del biglietto under 18
+var under18 = parseInt(costo / 100 * 80);
 
-// calcolo del costo del biglietto se utente minorenne o over 65;
-costo if (eta < 18){
-  costo / 100 * 80;
-} else if (eta > 65){
-  costo / 100 * 60;
-};
+console.log(under18);
+
+// calcolo costo biglietto over 65
+var over65 = parseInt(costo / 100 * 60);
+
+console.log(over65);
+
+// // // calcolo del costo del biglietto se utente minorenne o over 65;
+// costo if (eta < 18){
+//   under18;
+// }
